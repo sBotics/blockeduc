69da0b97b269d32273de2a4e8a5facbc7a6d3420
Julio Cesar Vera Neto <neto-vera@hotmailk.com>
2021-10-02 18:54:57 -0300

    3.0.0

--- src/class/load-check.js
+++ src/class/load-check.js
@@ -1,27 +1,50 @@
-const events = 4;
 let eventsSuccess = 0;
+
+const events = 4;
+const fullProgressBar = 100;
 
 export class LoadCheck {
     constructor(base = true) {
         if (base) {
             this.state = "processing";
-            document.getElementById('__LOAD_MOSS_InternetConnection').style.backgroundColor = this.colors();
-            document.getElementById('__LOAD_MOSS_ServerConnection').style.backgroundColor = this.colors();
-            document.getElementById('__LOAD_MOSS_UserLoad').style.backgroundColor = this.colors();
-            document.getElementById('__LOAD_MOSS_SearchAndLoadProgram').style.backgroundColor = this.colors();
+            this.changeColorLoading('__LOAD_MOSS_InternetConnection');
+            this.changeColorLoading('__LOAD_MOSS_ServerConnection');
+            this.changeColorLoading('__LOAD_MOSS_UserLoad');
+            this.changeColorLoading('__LOAD_MOSS_SearchAndLoadProgram');
         }
     }
 
     events(objectID, state) {
         this.state = state;
+        eventsSuccess++;
+        this.disabledColorLoading(objectID);
+        this.progressBar();
         document.getElementById(objectID).style.backgroundColor = this.colors();
+    }
+
+    changeColorLoading(objectID) {
+        document.getElementById(objectID).getElementsByClassName('double-bounce1')[0].style.backgroundColor = this.colors()
+        document.getElementById(objectID).getElementsByClassName('double-bounce2')[0].style.backgroundColor = this.colors()
+    }
+
+    disabledColorLoading(objectID) {
+        document.getElementById(objectID).getElementsByClassName('double-bounce1')[0].style.display = 'none'
+        document.getElementById(objectID).getElementsByClassName('double-bounce2')[0].style.display = 'none'
+    }
+
+    colors() {
+        return this.state === "processing" ? '#0021a7' : this.state ? '#2CD25E' : '#a70000';
     }
 
     isComplete() {
         return eventsSuccess >= events;
     }
 
-    colors() {
-        return this.state === "processing" ? '#0021a7' : this.state ? '#00a70c' : '#a70000';
+    progressBar(){
+        if(!this.isComplete()){
+            let percentage = fullProgressBar / events;
+            let bar = document.getElementById('__PROGRESSBAR_progress');
+            bar.style.width = `${percentage*eventsSuccess}%`;
+        }
     }
 }
